1994d69adc49c6bf9f2da8fb6091cbd14886215b
Christina George <145782012+christinawdc@users.noreply.github.com>
2025-02-19 15:03:42 +0530

    Create displayStudentDetails.sh

---
+++ Shell Programming/displayStudentDetails.sh
@@ -0,0 +1,16 @@
+//A shell program to display student details.
+echo "Enter name:";
+read name;
+echo "Enter semester:";
+read semester;
+echo "Enter the batch:";
+read batch;
+echo "Enter roll no:";
+read rollno;
+echo "Enter register no:";
+read reg;
+echo "Name: $name";
+echo "Semester: $semester";
+echo "Batch : $batch";
+echo "Roll No: $rollno";
+echo "Register No: $reg";
