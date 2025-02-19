fab75f0424cea9b8cf67df60b7861ccdd30f6d34
Christina George <145782012+christinawdc@users.noreply.github.com>
2025-02-19 15:06:25 +0530

    Create biggestof2numbers.sh

---
+++ Shell Programming/biggestof2numbers.sh
@@ -0,0 +1,9 @@
+//Shell program to display the biggest of two numbers.
+echo "Enter first number:";
+read a;
+echo "Enter second number:";
+read b;
+if ((a>b))
+then echo "Biggest : $a" ;
+else echo "Biggest : $b";
+fi;
